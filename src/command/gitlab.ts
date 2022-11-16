33c1a615b38937dca10a9c57dedf9ab8b7d877e6
daisuke-tanabe <dev.daisuke.tanabe@gmail.com>
2022-11-16 14:36:55 +0900

    feat: add the date elapsed to the display

--- src/command/gitlab.ts
+++ src/command/gitlab.ts
@@ -1,6 +1,7 @@
 import { Command } from 'https://deno.land/x/cliffy@v0.25.4/command/mod.ts';
 import { Select, Checkbox } from 'https://deno.land/x/cliffy@v0.25.4/prompt/mod.ts';
 import { Table, Cell } from "https://deno.land/x/cliffy@v0.25.4/table/mod.ts";
+import { difference } from "https://deno.land/std@0.164.0/datetime/mod.ts";
 import {Artifact, Branch, FetchRepositoryBranchesByProject } from './types.ts';
 import assertIsDefined from '../lib/assertIsDefined.ts'
 import config from '../../config.json' assert { type: 'json' };
@@ -18,6 +19,7 @@ const fetchBranches: FetchRepositoryBranchesByProject<Branch[]> = ({ entry, toke
 const gitlab = new Command()
   .action(async () => {
     const gitlabConfig = config.gitlab;
+    // const today = format(new Date(), 'yyyy-MM-ddTHH:mm:ss.SSS+09:00');
 
     // https://cliffy.io/docs@v0.25.4/prompt/types/select
     const selectedGitlab = await Select.prompt({
@@ -55,12 +57,45 @@ const gitlab = new Command()
     artifact.projects.forEach(({ siteName, branches }) => {
       const mappedBranches = branches.map(branch => {
         const { name, commit, merged, protected: guard } = branch;
+        // NOTE: 日付の実装はざっくりなのでいつか見直すかも
+        const { years, weeks, days, hours, seconds } = difference(
+          new Date(),
+          new Date(commit.created_at),
+          {
+            units: ['years', 'weeks', 'days', 'hours', 'seconds']
+          });
+        const diffDateToArray = [
+          { key: 'years', value: years as number },
+          { key: 'weeks', value: weeks as number },
+          { key: 'days', value: days as number },
+          { key: 'hours', value: hours as number },
+          { key: 'seconds', value: seconds as number }
+        ];
+        const elapsedDate = diffDateToArray.find(({ value }) => value > 0);
+
+        // findの返り値を確認
+        assertIsDefined(elapsedDate);
+
+        const dateTransform = (elapsedDate: { key: string, value: number }): { key: string, value: number } => {
+          // 1ヶ月は4.34524週間なのでmonthが0でweekが4より上なら計算した結果の月を返す
+          if (elapsedDate.key === 'weeks' && elapsedDate.value > 4) {
+            return {
+              key: 'month',
+              value: Math.trunc(elapsedDate.value / 4.34524),
+            }
+          }
+          return elapsedDate;
+        }
+
+        const timeAgo = dateTransform(elapsedDate);
+
         return {
           branch_name: name,
           committer_name: commit.author_name,
           committed_date: commit.created_at,
           merged: `${merged}`,
           protected: `${guard}`,
+          time_ago: `${timeAgo.value} ${timeAgo.key} ago`
         }
       });
       const flattenBranches = mappedBranches.map(branch => Object.entries(branch).map(([key, value]) => ({ key, value })));
